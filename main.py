5493abda5b02fae3b4c46cb46f90ff5b477a09a8
kayuprog <kayuprog@gmail.com>
2024-08-29 18:35:37 +0900

    update.pyを追加し，textのupdateを分けた．

--- main.py
+++ main.py
@@ -7,43 +7,47 @@ import gc
 #########################################################
 import scraping 
 
-result=scraping.scrape()
+def make_text():
+    result=scraping.scrape()
 
-text_time=[]
-text_umb=None
+    text_time=[]
+    text_umb=None
 
-if (result['yowa']!=None or result['ame']!=None or result['tuyo']!=None or result['gou']!=None):
-    text_umb="今日は傘を持っていきましょう．"
-elif result['ko']!=None and (result['yowa']==None and result['ame']==None and result['tuyo']==None and result['gou']==None):
-    text_umb="今日は折り畳み傘を持っていきましょう．"
-elif not any(result):
-    text_umb="今日は傘を持って行かなくて大丈夫です．"
+    if (result['yowa']!=None or result['ame']!=None or result['tuyo']!=None or result['gou']!=None):
+        text_umb="今日は傘を持っていきましょう．"
+    elif result['ko']!=None and (result['yowa']==None and result['ame']==None and result['tuyo']==None and result['gou']==None):
+        text_umb="今日は折り畳み傘を持っていきましょう．"
+    elif not any(result):
+        text_umb="今日は傘を持って行かなくて大丈夫です．"
 
-keys=list(result.keys())
+    keys=list(result.keys())
 
-for key in keys:
-    if result[key]!=None:
-        if key=='ko':
-            time=str(result['ko'])+"時から小雨．"
-        elif key=='yowa':
-            time=str(result['yowa'])+"時から弱雨．"
-        elif key=='ame':
-            time=str(result['ame'])+"時から雨．"
-        elif key=='tuyo':
-            time=str(result['tuyo'])+"時から強雨．"
-        elif key=='gou':
-            time=str(result['gou'])+"時から豪雨．"
+    for key in keys:
+        if result[key]!=None:
+            if key=='ko':
+                time=str(result['ko'])+"時から小雨．"
+            elif key=='yowa':
+                time=str(result['yowa'])+"時から弱雨．"
+            elif key=='ame':
+                time=str(result['ame'])+"時から雨．"
+            elif key=='tuyo':
+                time=str(result['tuyo'])+"時から強雨．"
+            elif key=='gou':
+                time=str(result['gou'])+"時から豪雨．"
 
-        text_time.append(time)
+            text_time.append(time)
 
-text_full=text_umb+''.join(text_time)+"です．"
+    text_full=text_umb+''.join(text_time)+"です．"
+    return text_full
 
 ################# 音声ファイル作成 #######################
 import os
 from gtts import gTTS
-japanese=text_full
-tts = gTTS(japanese, lang='ja')
-tts.save("./audio/readaloud.mp3")
+
+def make_audio(text):
+    japanese=text
+    tts = gTTS(japanese, lang='ja')
+    tts.save("./audio/readaloud.mp3")
 
 
 
@@ -51,26 +55,27 @@ tts.save("./audio/readaloud.mp3")
 # TCP connection
 #########################################################
 
-HOST = '192.168.0.12'  # Raspberry PiのIPアドレス
-PORT = 51000           # クライアントと同じポート番号
+def pico_connect():
+    HOST = '192.168.0.12'  # Raspberry PiのIPアドレス
+    PORT = 51000           # クライアントと同じポート番号
 
 
-# ソケットの設定
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((HOST, PORT))
-sock.listen(1)
+    # ソケットの設定
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((HOST, PORT))
+    sock.listen(1)
 
-print('Waiting for a connection...')
+    print('Waiting for a connection...')
 
 
 
-# 接続の確立
-conn, addr = sock.accept()
-print(f'Connected by {addr}')
+    # 接続の確立
+    conn, addr = sock.accept()
+    print(f'Connected by {addr}')
 
-# ソケットを閉じる（通常は無限ループなので到達しません）
-sock.close()
+    # ソケットを閉じる（通常は無限ループなので到達しません）
+    sock.close()
 
 
 
@@ -79,7 +84,8 @@ sock.close()
 #########################################################
 
 ################# Japanese ###################
-os.system("mplayer -speed 1.4 -af scaletempo ./audio/readaloud.mp3")
+def read_aloud():
+    os.system("mplayer -speed 1.4 -af scaletempo ./audio/readaloud.mp3")
 
 # ################# English ###################
 # english='Japan\'s Health Ministry updated its Q&A page. You can find answers to such questions as how you can avoid catching/spreading the virus, what is the "cough etiquette". '
@@ -131,17 +137,33 @@ os.system("mplayer -speed 1.4 -af scaletempo ./audio/readaloud.mp3")
 #     main()
 
 
+#################################
+# 実行
+#################################
+import schedule
+import time
+from datetime import datetime
+
+def task1():#audioの作成まで行う．
+     text_full=make_text()
+     make_audio(text_full)
+     gc.collect()#ガベージコレクションの強制実行
+
+
+def task2():#接続を待って読み上げまで
+    pico_connect()
+    read_aloud()
+    gc.collect()#ガベージコレクションの強制実行
+
+
+
+while True:
+    task2()
+    if datetime.now().hour==3:
+        break#毎朝3時でmain.py終了
+     
+
+
+
+
 gc.collect()#ガベージコレクションの強制実行
-
-
-
-
-
-
-
-
-
-
-
-
-
